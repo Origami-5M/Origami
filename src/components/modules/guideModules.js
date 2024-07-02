f4d51d78fe4473b84a69a8b8364b377853de03d7
Joohyeji <rosyjoo1999@gmail.com>
2024-07-02 12:13:38 +0900

    🐛 Fix: #60 가이드모드 스텝 넘어가는 버그 수정

--- src/components/modules/guideModules.js
+++ src/components/modules/guideModules.js
@@ -20,10 +20,14 @@ const checkUnfoldButtons = () => {
 };
 
 const updateStep = step => {
-  nowStep += step;
-  goToSlide(nowStep);
-  checkUnfoldButtons();
-  changeBorderVertices();
+  const newStep = nowStep + step;
+
+  if (newStep >= 0 && newStep < guideStep.length) {
+    nowStep = newStep;
+    goToSlide(nowStep);
+    checkUnfoldButtons();
+    changeBorderVertices(guideStep[nowStep].points);
+  }
 };
 
 const updateZPosition = vertex => {
